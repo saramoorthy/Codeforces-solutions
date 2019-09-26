d76a021cf9a3a67d261c82ca6c868f7778583c40
Saraswathi Shanmugamoorthy <saraleo369@gmail.com>
2019-09-25 23:42:48 -0500

    added 755A

---
+++ 755A.cpp
@@ -0,0 +1,13 @@
+//755A
+
+//https://codeforces.com/problemset/problem/755/A
+
+
+#include<bits/stdc++.h>
+int main()
+{
+    int n;
+    std::cin>>n;
+    std::cout<<(n>2?n-2:n+2);
+    return 0;
+    }

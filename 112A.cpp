4786cc7c9db423e82551d1a09d54d87635c87f29
Leonid369 <sara.moorthy369@gmail.com>
2020-08-06 19:03:02 -0500

    added new

--- 112A.cpp
+++ 112A.cpp
@@ -9,34 +9,36 @@
 //Output
 //If the first string is less than the second one, print "-1". If the second string is less than the first one, print "1". If the strings are equal, print "0". Note that the letters' case is not taken into consideration when the strings are compared.
 
-#include <iostream>
-#include <math.h>
+#include<iostream>
+#include<string>
 using namespace std;
 
-int main() {
-    int ans=0;
-    string str1,str2;
-    cin>>str1>>str2;
-    int len=str1.length();
+int main(){
+
+    string str1, str2;
+    int i;
+    cin>>str1;
+    cin>>str2;
+    int len = str1.length();
     for(int i=0;i<len;i++){
         str1[i]=tolower(str1[i]);
         str2[i]=tolower(str2[i]);
     }
-    for(int i=0;i<len;i++)
-    {
-        if((str1[i]-str2[i])==0 || abs(str1[i]-str2[i])==32)
-        {
-            ans=ans+0;
-        }else if((str1[i]-str2[i])>0 ){
-            ans=1;
-            break;
-        }else if((str1[i]-str2[i])<0 ){
-            ans=-1;
+    for( i=0;i<str1.length(); i++){
+        if(str1[i]-str2[i]>0){
+            cout<<1<<endl;
             break;
         }
+        else if(str1[i]-str2[i]<0){
+            cout<<-1<<endl;
+            break;
+        }
+        else
+            continue;
     }
-    
-    cout<<ans;
-    
+
+    if(i== str1.length())
+        cout<<0<<endl;
+
     return 0;
 }

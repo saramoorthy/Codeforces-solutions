2a740bf6f60669e80777ae2cd3686adf0a3d70fe
Leonid369 <sara.moorthy369@gmail.com>
2020-08-08 07:35:33 -0500

    added 58A.cpp

--- 58A.cpp
+++ 58A.cpp
@@ -1,34 +1,21 @@
-
-//58A - Chat room
-//
-//https://codeforces.com/problemset/problem/58/A
-//
-//Vasya has recently learned to type and log on to the Internet. He immediately entered a chat room and decided to say hello to everybody. Vasya typed the word s. It is considered that Vasya managed to say hello if several letters can be deleted from the typed word so that it resulted in the word "hello". For example, if Vasya types the word "ahhellllloou", it will be considered that he said hello, and if he types "hlelo", it will be considered that Vasya got misunderstood and he didn't manage to say hello. Determine whether Vasya managed to say hello by the given word s.
-//
-//Input
-//The first and only line contains the word s, which Vasya typed. This word consisits of small Latin letters, its length is no less that 1 and no more than 100 letters.
-//
-//Output
-//If Vasya managed to say hello, print "YES", otherwise print "NO".
-
-
-#include <iostream>
+#include<iostream>
+#include<stack>
 
 using namespace std;
-
-int main() {
-    string str,str2="hello";
+int main(){
+    
+    string str;
+    string ref = "hello";
     int pass=0;
     cin>>str;
-    int n=str.length();
     int j=0;
-    for(int i=0;i<n;i++){
-        if(str[i]==str2[j])
-        {
+    for(int i=0;i<str.length();i++){
+        
+        if(str[i]==ref[j]){
             j++;
             pass++;
+            
         }
-        
     }
     if(pass==5)
         cout<<"YES"<<endl;
@@ -36,4 +23,3 @@ int main() {
         cout<<"NO"<<endl;
     return 0;
 }
-

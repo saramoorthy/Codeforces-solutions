eca207fce407d967d7436c9aca751bab79b12ebb
Leonid369 <sara.moorthy369@gmail.com>
2020-08-08 18:40:31 -0500

    added 69A.cpp

--- 59A.cpp
+++ 59A.cpp
@@ -1,42 +1,34 @@
-//59A - Word
-//
-//https://codeforces.com/problemset/problem/59/A
-//
-//Vasya is very upset that many people on the Net mix uppercase and lowercase letters in one word. That's why he decided to invent an extension for his favorite browser that would change the letters' register in every word so that it either only consisted of lowercase letters or, vice versa, only of uppercase ones. At that as little as possible letters should be changed in the word. For example, the word HoUse must be replaced with house, and the word ViP — with VIP. If a word contains an equal number of uppercase and lowercase letters, you should replace all the letters with lowercase ones. For example, maTRIx should be replaced by matrix. Your task is to use the given method on one given word.
-//
-//Input
-//The first line contains a word s — it consists of uppercase and lowercase Latin letters and possesses the length from 1 to 100.
-//
-//Output
-//Print the corrected word s. If the given word s has strictly more uppercase letters, make the word written in the uppercase register, otherwise - in the lowercase one.
-
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<iostream>
+#include<string>
 using namespace std;
 
-int main() {
+int main(){
     string str;
-    int lcount=0,ucount=0;
+    int lower=0;
+    int upper=0;
+   
     cin>>str;
-    int n=str.length();
-    for(int i=0;i<n;i++){
-        if(isupper(str[i]))
-            ucount++;
-        else if(islower(str[i]))
-            lcount++;
-    }
-    if(lcount<ucount){
-        for(int i=0;i<n;i++){
-            str[i]=toupper(str[i]);}
-    }
-    else if((ucount<lcount)||(ucount==lcount)){
-        for(int i=0;i<n;i++){
-            str[i]=tolower(str[i]);}
+    
+    for(char &c:str){
+        
+        if(isupper(c))
+            upper++;
+        else
+            lower++;
     }
     
+    if(upper<=lower){
+        for(char &x:str)
+            x=tolower(x);
+        
+    }
+    else
+    {
+        for(char &y:str)
+            y=toupper(y);
+    }
     cout<<str<<endl;
     
-    
+
     return 0;
 }

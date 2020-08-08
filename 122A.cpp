4d573d5b01dcc5ade68bf0829a2675a824a01f9c
Leonid369 <sara.moorthy369@gmail.com>
2020-08-08 09:53:47 -0500

    added 122A.cpp

--- 122A.cpp
+++ 122A.cpp
@@ -1,50 +1,38 @@
-//122A - Lucky Division
-//
-//https://codeforces.com/problemset/problem/122/A
-//
-//
-//Petya loves lucky numbers. Everybody knows that lucky numbers are positive integers whose decimal representation contains only the lucky digits 4 and 7. For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
-//
-//Petya calls a number almost lucky if it could be evenly divided by some lucky number. Help him find out if the given number n is almost lucky.
-//
-//Input
-//The single line contains an integer n (1 ≤ n ≤ 1000) — the number that needs to be checked.
-//
-//Output
-//In the only line print "YES" (without the quotes), if number n is almost lucky. Otherwise, print "NO" (without the quotes).
-
-#include <iostream>
-
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int main() {
+
+int main(){
     int n;
-    int x;
     cin>>n;
-    string str;
+    vector<int> lucky;
+    int flag=0;
+
     for(int i=1;i<=n;i++){
-        x=i;
-        while(x>0){
-            if((x%10==4)||(x%10==7)){
-                x=x/10;
-            }
-            else
+        int j = i;
+        while(j>0){
+            if(j%10 == 4 || j%10 == 7){
+                j=j/10;
+                
+            }else
                 break;
         }
-        if(x<1){
-            if(n%i == 0){
-                str="YES";
-            }
-            
-        }
-        
-        if(str=="YES"){
-            cout<<str<<endl;
-            break;
-        }
-        if(i>=n)
-            cout<<"NO"<<endl;
+        if(j==0)
+            lucky.push_back(i);
     }
+  
+    for( int x: lucky){
+        if(n%x == 0){
+            flag = 1;
+            break;
+    }
+    }
+    
+   if(flag)
+       cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
     
     
     return 0;

817ada23850eb00d79dccbb3e11d026fd29e815e
Leonid369 <sara.moorthy369@gmail.com>
2020-08-09 10:00:00 -0500

    added 41A

--- 41A.cpp
+++ 41A.cpp
@@ -1,27 +1,27 @@
-//41A - Translation
-//
-//https://codeforces.com/contest/41/problem/A
-//
-//The translation from the Berland language into the Birland language is not an easy task. Those languages are very similar: a berlandish word differs from a birlandish word with the same meaning a little: it is spelled (and pronounced) reversely. For example, a Berlandish word code corresponds to a Birlandish word edoc. However, it's easy to make a mistake during the «translation». Vasya translated word s from Berlandish into Birlandish as t. Help him: find out if he translated the word correctly.
-//
-//Input
-//The first line contains word s, the second line contains word t. The words consist of lowercase Latin letters. The input data do not consist unnecessary spaces. The words are not empty and their lengths do not exceed 100 symbols.
-//
-//Output
-//If the word t is a word s, written reversely, print YES, otherwise print NO.
-
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<iostream>
 using namespace std;
 
-int main() {
-    string str1,str2;
+int main(){
+    
+    string str1, str2;
     cin>>str1>>str2;
-    reverse(str1.begin(), str1.end());
-    if(str1==str2)
-        cout<<"YES"<<endl;
-    else
+    int len = str1.length();
+    int i=0;
+    int flag=0;
+    int j= len-1;
+    while(i<len && j>=0){
+        
+        if(str1[i]==str2[j]){
+            i++;
+            j--;
+        }else{
+            flag=1;
+            break;}
+    }
+    if(flag)
         cout<<"NO"<<endl;
+    else
+        cout<<"YES"<<endl;
+    
     return 0;
 }

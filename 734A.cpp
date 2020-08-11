5cc96dd44c0a4a3f0befa6f3444e5515ff06880f
Leonid369 <sara.moorthy369@gmail.com>
2020-08-11 13:13:23 -0500

    added 734A.cpp

--- 734A.cpp
+++ 734A.cpp
@@ -1,51 +1,30 @@
-//734A - Anton and Danik
-//
-//
-//https://codeforces.com/problemset/problem/734/A
-//
-//
-//Anton likes to play chess, and so does his friend Danik.
-//
-//Once they have played n games in a row. For each game it's known who was the winner — Anton or Danik. None of the games ended with a tie.
-//
-//Now Anton wonders, who won more games, he or Danik? Help him determine this.
-//
-//Input
-//The first line of the input contains a single integer n (1 ≤ n ≤ 100 000) — the number of games played.
-//
-//The second line contains a string s, consisting of n uppercase English letters 'A' and 'D' — the outcome of each of the games. The i-th character of the string is equal to 'A' if the Anton won the i-th game and 'D' if Danik won the i-th game.
-//
-//Output
-//If Anton won more games than Danik, print "Anton" (without quotes) in the only line of the output.
-//
-//If Danik won more games than Anton, print "Danik" (without quotes) in the only line of the output.
-//
-//If Anton and Danik won the same number of games, print "Friendship" (without quotes).
-
-
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int n,acount=0,dcount=0;
-    cin>>n;
+int main(){
+    
+    int n;
     string str;
+    int countA=0;
+    int countD=0;
+    cin>>n;
     cin>>str;
-    for(int i=0;i<n;i++){
-        if(str[i]=='D')
-            dcount++;
+    
+    for(int i=0; i<n; i++){
+        
         if(str[i]=='A')
-            acount++;
+            countA++;
+        if(str[i]=='D')
+            countD++;
+        
     }
-    if(acount>dcount)
+    
+    if(countA>countD)
         cout<<"Anton"<<endl;
-    else if(dcount>acount)
+    else if(countD>countA)
         cout<<"Danik"<<endl;
     else
         cout<<"Friendship"<<endl;
-    
     
     return 0;
 }

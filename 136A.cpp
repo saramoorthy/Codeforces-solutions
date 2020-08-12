d286a160c056bb5ca82496744778c0009c8000be
Leonid369 <sara.moorthy369@gmail.com>
2020-08-12 02:28:10 -0500

    added 136A.cpp

--- 136A.cpp
+++ 136A.cpp
@@ -1,39 +1,30 @@
-//136A Presents
-//
-//https://codeforces.com/problemset/problem/136/A
-//
-//
-//Little Petya very much likes gifts. Recently he has received a new laptop as a New Year gift from his mother. He immediately decided to give it to somebody else as what can be more pleasant than giving somebody gifts. And on this occasion he organized a New Year party at his place and invited n his friends there.
-//
-//If there's one thing Petya likes more that receiving gifts, that's watching others giving gifts to somebody else. Thus, he safely hid the laptop until the next New Year and made up his mind to watch his friends exchanging gifts while he does not participate in the process. He numbered all his friends with integers from 1 to n. Petya remembered that a friend number i gave a gift to a friend number pi. He also remembered that each of his friends received exactly one gift.
-//
-//Now Petya wants to know for each friend i the number of a friend who has given him a gift.
-//
-//Input
-//The first line contains one integer n (1 ≤ n ≤ 100) — the quantity of friends Petya invited to the party. The second line contains n space-separated integers: the i-th number is pi — the number of a friend who gave a gift to friend number i. It is guaranteed that each friend received exactly one gift. It is possible that some friends do not share Petya's ideas of giving gifts to somebody else. Those friends gave the gifts to themselves.
-//
-//Output
-//Print n space-separated integers: the i-th number should equal the number of the friend who gave a gift to friend number i.
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<map>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin>>n;
-    int m[n];
-    
-    for(int i=1;i<=n;i++){
-        int a=0;
-        cin>>a;
-        m[a-1]=i;
+    map<int, int> gift;
+    for(int i=1;i<n+1;i++){
+        int person;
+        cin>>person;
+        gift[i]=person;
+        
     }
-    for (int x:m)
-        cout<<x<<" ";
+    int k=1;
+    for(int x=0;x<n;x++){
+        
+    for(auto &j : gift){
+        int key=0;
+        if(j.second == k){
+            key = j.first;
+            cout<<key<<" ";
+        }
+    }
+        k++;
+    }
     cout<<endl;
     return 0;
 }
-
-

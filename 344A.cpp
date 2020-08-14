e6066ca50e390726398b823c8e813659d3e35113
Leonid369 <sara.moorthy369@gmail.com>
2020-08-13 19:38:16 -0500

    added 344A

--- 344A.cpp
+++ 344A.cpp
@@ -1,41 +1,28 @@
-//344A - Magnets
-//
-//https://codeforces.com/problemset/problem/344/A
-//
-//Mad scientist Mike entertains himself by arranging rows of dominoes. He doesn't need dominoes, though: he uses rectangular magnets instead. Each magnet has two poles, positive (a "plus") and negative (a "minus"). If two magnets are put together at a close distance, then the like poles will repel each other and the opposite poles will attract each other.
-//
-//Mike starts by laying one magnet horizontally on the table. During each following step Mike adds one more magnet horizontally to the right end of the row. Depending on how Mike puts the magnet on the table, it is either attracted to the previous one (forming a group of multiple magnets linked together) or repelled by it (then Mike lays this magnet at some distance to the right from the previous one). We assume that a sole magnet not linked to others forms a group of its own.
-//
-//
-//Mike arranged multiple magnets in a row. Determine the number of groups that the magnets formed.
-//
-//Input
-//The first line of the input contains an integer n (1 ≤ n ≤ 100000) — the number of magnets. Then n lines follow. The i-th line (1 ≤ i ≤ n) contains either characters "01", if Mike put the i-th magnet in the "plus-minus" position, or characters "10", if Mike put the magnet in the "minus-plus" position.
-//
-//Output
-//On the single line of the output print the number of groups of magnets.
-
-#include <iostream>
-#include <math.h>
+#include<iostream>
 using namespace std;
-
-int main() {
-    int n,groups=1;
-    int prev,a;
+int main(){
+    
+    int n;
+    int groups=0;
     cin>>n;
-    cin>>prev;
-    int i=n-1;
-    while(i){
-        cin>>a;
-        if(a==prev){
-            i--;
-            prev=a;
-        }else{
+    string first;
+    cin>>first;
+    char prev=first[1];
+    for(int i=1; i<n; i++){
+    
+        string str;
+        cin>>str;
+        
+        if(str[0]==prev)
             groups++;
-            i--;
-            prev=a;
-        }
+        
+        prev=str[1];
+        
+        
     }
-    cout<<groups<<endl;
+    
+    cout<<groups+1<<endl;
+    
+    
     return 0;
 }
